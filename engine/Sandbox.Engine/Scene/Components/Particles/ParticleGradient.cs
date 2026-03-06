07b069fe12b261e8f1982f74e87ce30600a8586f
James King <james.king@facepunchstudios.com>
2026-03-06 15:37:48 +0000

    Simplify how ParticleGradients get serialized (#4252)

--- engine/Sandbox.Engine/Scene/Components/Particles/ParticleGradient.cs
+++ engine/Sandbox.Engine/Scene/Components/Particles/ParticleGradient.cs
@@ -1,9 +1,11 @@
 ﻿using System.Runtime.CompilerServices;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace Sandbox;
 
 [Expose]
+[JsonConverter( typeof( ParticleGradientConverter ) )]
 public struct ParticleGradient
 {
 	public ParticleGradient()
@@ -18,7 +20,6 @@ public struct ParticleGradient
 	public Color ConstantA { readonly get; set; } = Color.White;
 	public Color ConstantB { readonly get; set; } = Color.White;
 
-	[JsonIgnore]
 	public Color ConstantValue
 	{
 		readonly get => ConstantA;
@@ -83,5 +84,81 @@ public struct ParticleGradient
 	public readonly Color Evaluate( Particle p, int seed, [CallerLineNumber] int line = 0 )
 	{
 		return Evaluate( p.LifeDelta, p.Rand( seed, line ) );
+	}
+}
+
+/// <summary>
+/// <see cref="JsonConverter"/> for <see cref="ParticleGradient"/> that omits unused properties.
+/// Will serialize as just a <see cref="Color"/> string for <see cref="ParticleGradient.ValueType.Constant"/>.
+/// </summary>
+file sealed class ParticleGradientConverter : JsonConverter<ParticleGradient>
+{
+	private struct Model
+	{
+		public ParticleGradient.ValueType Type { get; init; }
+		public ParticleGradient.EvaluationType Evaluation { get; init; }
+
+		[JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
+		public Color? ConstantA { get; set; }
+		[JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
+		public Color? ConstantB { get; set; }
+
+		[JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
+		public Gradient? GradientA { get; set; }
+		[JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
+		public Gradient? GradientB { get; set; }
+	}
+
+	public override ParticleGradient Read( ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options )
+	{
+		// If a string, read as a Constant
+
+		if ( reader.TokenType == JsonTokenType.String )
+		{
+			return JsonSerializer.Deserialize<Color>( ref reader, options );
+		}
+
+		var model = JsonSerializer.Deserialize<Model>( ref reader, options );
+
+		return new ParticleGradient
+		{
+			Type = model.Type,
+			Evaluation = model.Evaluation,
+			ConstantA = model.ConstantA ?? Color.White,
+			ConstantB = model.ConstantB ?? Color.White,
+			GradientA = model.GradientA ?? Color.White,
+			GradientB = model.GradientB ?? Color.White
+		};
+	}
+
+	public override void Write( Utf8JsonWriter writer, ParticleGradient value, JsonSerializerOptions options )
+	{
+		// If just a Constant, write as a color string
+
+		if ( value.Type == ParticleGradient.ValueType.Constant )
+		{
+			JsonSerializer.Serialize( writer, value.ConstantValue, options );
+			return;
+		}
+
+		var model = new Model { Type = value.Type, Evaluation = value.Evaluation };
+
+		switch ( value.Type )
+		{
+			case ParticleGradient.ValueType.Range:
+				model.ConstantA = value.ConstantA;
+				model.ConstantB = value.ConstantB;
+				break;
+
+			case ParticleGradient.ValueType.Gradient:
+				model.GradientA = value.GradientA;
+				model.GradientB = value.GradientB;
+				break;
+
+			default:
+				throw new NotImplementedException( $"Serializing {value.Type} not implemented yet." );
+		}
+
+		JsonSerializer.Serialize( writer, model, options );
 	}
 }
